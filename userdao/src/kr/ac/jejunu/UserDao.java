3b4ace0e8d3d93aa334a3e1dd8741db182920898
starsieun <risieun513@naver.com>
2017-04-21 09:49:17 +0900

    userdao_refactoring_1

--- userdao/src/kr/ac/jejunu/UserDao.java
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -18,25 +18,14 @@ public class UserDao {
 
     public User get(String id) throws ClassNotFoundException, SQLException {
 
-        //Class 로딩해야하네
-        Class.forName("com.mysql.jdbc.Driver");
 
-        //커넥션맺기
+        Connection connection = getConnection();
 
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.106/sieun1","root","1234");
-
-            //쿼리만들기
         PreparedStatement preparedStatement = connection.prepareStatement("select * from user where id = ?");
         preparedStatement.setString(1, id);
 
-
-
-
-            //쿼리실행
-
         ResultSet resultSet = preparedStatement.executeQuery();
-            //생성된결과를 객체 매핑
 
         resultSet.next();
         User user = new User();
@@ -45,13 +34,9 @@ public class UserDao {
         user.setName(resultSet.getString("name"));
         user.setPassword(resultSet.getString("password"));
 
-            //자원해지
-
         resultSet.close();
         preparedStatement.close();
         connection.close();
-
-            //결과를 리턴
 
         return user;
     }
@@ -59,34 +44,24 @@ public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        //Class 로딩해야하네
-        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = getConnection();
 
-        //커넥션맺기
-        Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.106/sieun1","root","1234");
-
-        //쿼리만들기
         PreparedStatement preparedStatement = connection.prepareStatement("insert into user(id, name, password) values(?, ?, ?)");
         preparedStatement.setString(1, user.getId());
         preparedStatement.setString(2, user.getName());
         preparedStatement.setString(3,user.getPassword());
 
 
-
-
-        //쿼리실행
-
         preparedStatement.executeUpdate();
 
-
-
-        //자원해지
         preparedStatement.close();
         connection.close();
 
-        //결과를 리턴
-
-
-
     }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://117.17.102.106/sieun1","root","1234");
+    }
+
 }
